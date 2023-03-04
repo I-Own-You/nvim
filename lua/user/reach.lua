d5ea02e6f4c06c62fd11691442fba7d0230c0798
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2023-03-04 21:28:37 +0200

    added comments for reach.lua functionality

--- lua/user/reach.lua
+++ lua/user/reach.lua
@@ -3,3 +3,9 @@ require('reach').setup({
   notifications = true,
 })
 
+
+-- space - delete buffers
+-- | - split vertically
+-- - split horrizontaly
+-- ] - open in new tab
+-- =(number) - assign priority starting (1 - highest, space - remove it)
