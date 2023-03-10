69fcbdb7bfb6cca822016dc96a3e28bc3ca0f557
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2023-03-10 23:52:55 +0200

    changed settings for session in neovim

--- lua/plugins/multiple-session.lua
+++ lua/plugins/multiple-session.lua
@@ -9,7 +9,7 @@ require("multiple-session").setup({
 	-- whether to auto load session when neovim start(if this session exists)
 	auto_load_session = true,
 	-- whether to auto load session at start when neovim opened with args
-	force_auto_load = true,
+	force_auto_load = false,
 	-- whether to auto save session when leave neovim(if this session exists)
 	auto_save_session = true,
 	-- whether to auto create session at leave if this session doesn't exist
@@ -19,7 +19,7 @@ require("multiple-session").setup({
 	-- default arg number
 	-- for neovim > v0.9, nvim command may have the default arg `--embed`
 	-- in this case, you have to set this to 2 for triggering session restoration correctly on startup
-	default_arg_num = 1,
+	default_arg_num = 2,
 	-- command to create directory
 	create_dir = "mkdir -p",
 	-- command to delete session file
