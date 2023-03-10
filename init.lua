50026f7a043dd5df7cff34fc723350b4ba55e70f
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2023-03-10 19:09:39 +0200

    splitted settings for user and plugins in neovim

--- init.lua
+++ init.lua
@@ -18,39 +18,39 @@ vim.g.python_highlight_func_calls = 1
 
 
 
-require 'user.colorscheme'
-require 'user.options'
-require 'user.keymaps'
-require 'user.plugins'
-require 'user.colorizer'
-require 'user.cmp'
-require 'user.lsp'
-require 'user.telescope'
-require 'user.treesiter'
-require 'user.autopairs'
-require "user.comment"
-require "user.gitsigns"
-require "user.nvim-tree"
-require "user.bufferline"
-require "user.toggleterm"
-require "user.lualine"
-require "user.symbols-outline"
-require "user.lsp-signature"
-require "user.colorful-winsep"
-require "user.wilder"
-require "user.indent_blankline"
-require "user.todo-comments"
-require "user.treesj"
-require "user.numb"
-require "user.pretty-fold"
-require "user.reach"
-require "user.nvim-dap-virtual-text"
-require "user.multiple-session"
-require "user.hop"
-require "user.luasnip-config"
+require "plugins.colorscheme"
+require "plugins.plugins"
+require "plugins.colorizer"
+require "plugins.cmp"
+require "plugins.lsp"
+require "plugins.telescope"
+require "plugins.treesiter"
+require "plugins.autopairs"
+require "plugins.comment"
+require "plugins.gitsigns"
+require "plugins.nvim-tree"
+require "plugins.bufferline"
+require "plugins.toggleterm"
+require "plugins.lualine"
+require "plugins.symbols-outline"
+require "plugins.lsp-signature"
+require "plugins.colorful-winsep"
+require "plugins.wilder"
+require "plugins.indent_blankline"
+require "plugins.todo-comments"
+require "plugins.treesj"
+require "plugins.numb"
+require "plugins.pretty-fold"
+require "plugins.reach"
+require "plugins.nvim-dap-virtual-text"
+require "plugins.multiple-session"
+require "plugins.hop"
+require "plugins.luasnip-config"
+require "user.options"
+require "user.keymaps"
 
 
--- require "user.dashboard"
+-- require "plugins.dashboard"
 
 -- vim.api.nvim_command("autocmd VimEnter * Dashboard")
 
