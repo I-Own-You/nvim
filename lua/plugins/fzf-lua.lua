46ae987a08d2b1acaf94b75217be7c1a0cea97fd
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2023-04-18 23:51:12 +0300

    added telescope startup profile to fzf-lua.lua file

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -3,6 +3,7 @@ require'fzf-lua'.setup {
     -- fzf_bin         = 'sk',            -- use skim instead of fzf?
     -- https://github.com/lotabout/skim
     -- can also be set to 'fzf-tmux'
+    'telescope',
     winopts = {
         -- split         = "belowright new",-- open in a split instead?
         -- "belowright new"  : split below
