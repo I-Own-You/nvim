f079235cf48e87aaf2494aa03bb5a93b4a7a2d99
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2023-05-08 20:35:14 +0300

    deleted open_file_path file

--- lua/open_file_path.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-function M.OpenCurrentFolder()
-  local buffer_name = vim.fn.bufname("%")
-  local cmd = "silent! !open " .. vim.fn.fnamemodify(buffer_name, ":h")
-  vim.api.nvim_command(cmd)
-end
-return M
