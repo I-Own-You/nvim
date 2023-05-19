4fbf36040f871ef1c4180c0804edd12951054f52
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2023-05-19 15:17:58 +0300

    changed sort option of bufferline.lua in nvim

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -100,7 +100,7 @@ bufferline.setup {
         show_buffer_close_icons = false,
         show_close_icon = true,
         show_tab_indicators = true,
-        persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+        persist_buffer_sort = false, -- whether or not custom sorted buffers should persist
         -- can also be a table containing 2 custom separators
         -- [focused and unfocused]. eg: { '|', '|' }
         separator_style = "slope", -- thick,thin,slant,padded_slant,slope, {"|", "|"} -- custom
