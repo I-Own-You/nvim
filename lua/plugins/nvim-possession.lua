ba0c1bd94245ac3018859284398ee9a968428b39
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2023-06-16 13:36:29 +0300

    change autoload to true for session

--- lua/plugins/nvim-possession.lua
+++ lua/plugins/nvim-possession.lua
@@ -5,7 +5,7 @@ sessions = {
         sessions_icon = ' 󱁿 ',
     },
 
-    autoload = false, -- whether to autoload sessions in the cwd at startup
+    autoload = true, -- whether to autoload sessions in the cwd at startup
     autosave = true, -- whether to autosave loaded sessions before quitting
     autoswitch = {
         enable = true, -- whether to enable autoswitch
