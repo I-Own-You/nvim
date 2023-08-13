e6651c5378db26c5bdcf8e82d34cb8def406c7ff
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2023-08-13 22:04:22 +0300

    added new color for foldcolumn

--- lua/plugins/nordic.lua
+++ lua/plugins/nordic.lua
@@ -1,6 +1,10 @@
+local palette = require 'nordic.colors'
 return {
 	italic_comments = true,
 	-- afdfsdaads
 	transparent_bg = false,
 	bold_keywords = true,
+  override = {
+    FoldColumn = { bg = "#242933", fg = "#28A761" }, -- 'foldcolumn'
+  }
 }
