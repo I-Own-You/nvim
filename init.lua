e6d0803d51624e21e9960dc6e44429348b6f82e4
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2023-12-22 14:57:56 +0200

    added block commenting for scss ft

--- init.lua
+++ init.lua
@@ -211,6 +211,8 @@ require("lazy").setup({
 			require("Comment").setup({
 				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 			})
+			local ft = require("Comment.ft")
+			ft.scss = { "// %s", "/* %s */" }
 		end,
 		lazy = false,
 	},
