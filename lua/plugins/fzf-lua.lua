e2a69f10d2f433711a93fefb5afca00b1cce970e
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2023-12-24 20:53:27 +0200

    changed to chafa again cuz i swithced to gnome

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -232,15 +232,14 @@ return {
 			-- will do nothing if `viu` isn't executable
 			extensions = {
 				-- neovim terminal only supports `viu` block output
-				-- ["png"] = { "chafa" },
-				-- ["jpg"] = { "chafa" },
-				-- ["svg"] = { "chafa" },
-				-- ["jpeg"] = { "chafa" },
-				-- ["svg"]       = { "chafa" },
-				["png"] = { "ueberzug" },
-				["jpg"] = { "ueberzug" },
-				["svg"] = { "ueberzug" },
-				["jpeg"] = { "ueberzug" },
+				["png"] = { "chafa" },
+				["jpg"] = { "chafa" },
+				["svg"] = { "chafa" },
+				["jpeg"] = { "chafa" },
+				-- ["png"] = { "ueberzug" },
+				-- ["jpg"] = { "ueberzug" },
+				-- ["svg"] = { "ueberzug" },
+				-- ["jpeg"] = { "ueberzug" },
 			},
 			-- if using `ueberzug` in the above extensions map
 			-- set the default image scaler, possible scalers:
