338c14050c9e97bb4fa29edf2212144ef5306271
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2024-01-29 21:03:28 +0200

    colorschee related

--- init.lua
+++ init.lua
@@ -63,6 +63,7 @@ require("lazy").setup({
 			hl(0, "IncSearch", { fg = "#ffffff", bg = "#FF007C" })
 			hl(0, "QuickFixLine", { fg = "NONE", bg = "NONE" })
 			hl(0, "QfLineNr", { fg = "#C4DE72", bg = "NONE" })
+			hl(0, "Visual", { fg = "NONE", bg = "#2c3333" })
 		end,
 	},
 	-- {
@@ -952,11 +953,18 @@ require("lazy").setup({
 			},
 			wrap = false, -- text wrap, only applies to floating window
 			--  install them: html, javascript, npm, react, express, react-router, css, typescript, python, django, tailwindcss, dom
-			previewer_cmd = "glow",
+			-- previewer_cmd = "glow",
 			cmd_args = { "-s", "dark", "-w", "80" },
-			-- cmd_ignore = {}, -- ignore cmd rendering for the listed docs
-			-- picker_cmd = false, -- use cmd previewer in picker preview
-			-- picker_cmd_args = {}, -- example using glow: { "-p" }
+			cmd_ignore = {}, -- ignore cmd rendering for the listed docs
+			picker_cmd = false, -- use cmd previewer in picker preview
+			picker_cmd_args = {}, -- example using glow: { "-s", "dark", "-w", "50" }
+			mappings = { -- keymaps for the doc buffer
+				open_in_browser = "",
+			},
+			ensure_installed = {}, -- get automatically installed
+			after_open = function(bufnr)
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "qq", ":close<CR>", {})
+			end, -- callback that runs after the Devdocs window is opened. Devdocs buffer ID will be passed in
 		},
 	},
 	{
