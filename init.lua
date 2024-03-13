db79dfbfd279db339bd1313267e3b94c31546adf
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2024-03-13 02:28:28 +0200

    added vim-dadbod-ui plugin

--- init.lua
+++ init.lua
@@ -1075,6 +1075,23 @@ require("lazy").setup({
 		end,
 		opts = require("plugins.before"),
 	},
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "tpope/vim-dadbod", lazy = true },
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			-- Your DBUI configuration
+			vim.g.db_ui_use_nerd_fonts = 1
+		end,
+	},
 })
 
 -- vim.cmd('highlight Search guifg=#F24211  guibg=#000000')
