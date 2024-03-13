027084e743f470f495f2a3a9146b57291d3e9a44
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2024-03-13 02:39:45 +0200

    changed bg color for lualine so it be as tokyonight bg

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,7 +6,8 @@ local lualine = require("lualine")
 -- Color table for highlights
 -- stylua: ignore
 local colors = {
-  bg       = '#202328',
+  -- bg       = '#202328',
+  bg       = '#1A1B26',
   fg       = '#bbc2cf',
   yellow   = '#ECBE7B',
   cyan     = '#008080',
