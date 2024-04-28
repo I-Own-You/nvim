de8f87dfce0bf103e7af6423b668241dc539cf47
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2024-04-28 19:51:21 +0300

    commented rg from cmp source and also added a property for treesitter to improve perforamce

--- lua/plugins/treesiter.lua
+++ lua/plugins/treesiter.lua
@@ -39,6 +39,7 @@ return {
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
+		additional_vim_regex_highlighting = false,
 		disable = { "" }, -- list of language that will be disabled
 	},
 	autopairs = {
