e728343a097366ac8fd7fddab7a99a194818ed84
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2024-07-04 04:30:21 +0300

    updated plugins

--- init.lua
+++ init.lua
@@ -81,6 +81,15 @@ require("lazy").setup({
 				"CmpItemAbbrDeprecatedDefault",
 				{ fg = "#525252", bg = "NONE", strikethrough = true }
 			)
+			vim.api.nvim_set_hl(0, "GitSignsAdd", { link = "GitSignsAdd" })
+			vim.api.nvim_set_hl(0, "GitSignsAddNr", { link = "GitSignsAddNr" })
+			vim.api.nvim_set_hl(0, "GitSignsAddLn", { link = "GitSignsAddLn" })
+			vim.api.nvim_set_hl(0, "GitSignsChange", { link = "GitSignsChange" })
+			vim.api.nvim_set_hl(0, "GitSignsChangeNr", { link = "GitSignsChangeNr" })
+			vim.api.nvim_set_hl(0, "GitSignsChangeLn", { link = "GitSignsChangeLn" })
+			vim.api.nvim_set_hl(0, "GitSignsDelete", { link = "GitSignsDelete" })
+			vim.api.nvim_set_hl(0, "GitSignsDeleteNr", { link = "GitSignsDeleteNr" })
+			vim.api.nvim_set_hl(0, "GitSignsDeleteLn", { link = "GitSignsDeleteLn" })
 		end,
 	},
 	{
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,12 +1,5 @@
 return {
 	debug_mode = true,
-	signs = {
-		add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-		change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-		delete = { hl = "GitSignsDelete", text = "▎", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		topdelete = { hl = "GitSignsDelete", text = "▎", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-	},
 	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 	numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
 	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
@@ -23,9 +16,6 @@ return {
 		delay = 100,
 		ignore_whitespace = false,
 	},
-	current_line_blame_formatter_opts = {
-		-- relative_time = false,
-	},
 	sign_priority = 6,
 	update_debounce = 100,
 	status_formatter = nil, -- Use default
@@ -37,8 +27,5 @@ return {
 		relative = "cursor",
 		row = 0,
 		col = 1,
-	},
-	yadm = {
-		enable = false,
 	},
 }
