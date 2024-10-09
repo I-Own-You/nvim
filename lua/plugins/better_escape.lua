03a00a0a11e69a4321d67b8a471f5ff00dbd6ef9
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2024-10-10 01:51:03 +0300

    formatted the file (stylua) and also turned off the jk for visual block

--- lua/plugins/better_escape.lua
+++ lua/plugins/better_escape.lua
@@ -1,34 +1,34 @@
 return {
-  timeout = vim.o.timeoutlen,
-  default_mappings = true,
-  mappings = {
-    i = {
-      j = {
-        -- These can all also be functions
-        k = "<Esc>",
-        j = "<Esc>",
-      },
-    },
-    c = {
-      j = {
-        k = "<Esc>",
-        j = "<Esc>",
-      },
-    },
-    t = {
-      j = {
-        k = false
-      }
-    },
-    v = {
-      j = {
-        k = "<Esc>",
-      },
-    },
-    s = {
-      j = {
-        k = "<Esc>",
-      },
-    },
-  },
+	timeout = vim.o.timeoutlen,
+	default_mappings = true,
+	mappings = {
+		i = {
+			j = {
+				-- These can all also be functions
+				k = "<Esc>",
+				j = "<Esc>",
+			},
+		},
+		c = {
+			j = {
+				k = "<Esc>",
+				j = "<Esc>",
+			},
+		},
+		t = {
+			j = {
+				k = false,
+			},
+		},
+		v = {
+			j = {
+				k = false,
+			},
+		},
+		s = {
+			j = {
+				k = "<Esc>",
+			},
+		},
+	},
 }
