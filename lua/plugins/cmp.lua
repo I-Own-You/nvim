3b643c3dd45a7d12bbb422ecfa4adba0671941a0
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2024-12-15 05:03:51 +0200

    turned on again cmdline completion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -235,19 +235,19 @@ cmp.setup.cmdline("?", {
 })
 
 -- `:` cmdline setup.
--- cmp.setup.cmdline(":", {
--- 	mapping = cmp.mapping.preset.cmdline(),
--- 	sources = cmp.config.sources({
--- 		{ name = "path" },
--- 	}, {
--- 		{
--- 			name = "cmdline",
--- 			option = {
--- 				ignore_cmds = {},
--- 			},
--- 		},
--- 	}),
--- })
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{
+			name = "cmdline",
+			option = {
+				ignore_cmds = {},
+			},
+		},
+	}),
+})
 
 -- return settings
 return vim.tbl_deep_extend("force", settings, require("nvchad.cmp"))
