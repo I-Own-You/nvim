a41e6411501ed546ec2f8b2641dd69365de3a8ee
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2024-12-19 01:01:50 +0200

    removed some unused options from blink cmp

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -31,24 +31,4 @@ return {
 			"fallback",
 		},
 	},
-	-- auto show doesnt work for me, idk why
-	-- documentation = {
-	-- 	auto_show = true,
-	-- 	auto_show_delay_ms = 100,
-	-- },
-	highlight = {
-		-- sets the fallback highlight groups to nvim-cmp's highlight groups
-		-- useful for when your theme doesn't support blink.cmp
-		-- will be removed in a future release, assuming themes add support
-		use_nvim_cmp_as_default = true,
-	},
-	-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-	-- adjusts spacing to ensure icons are aligned
-	nerd_font_variant = "mono",
-
-	-- experimental auto-brackets support
-	accept = { auto_brackets = { enabled = true } },
-
-	-- experimental signature help support
-	-- trigger = { signature_help = { enabled = true } },
 }
