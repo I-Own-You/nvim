9aed51663179b2c2d1069f9959e27ef061f611a3
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2025-02-11 04:04:42 +0200

    added html snippets to typescriptreact and javascriptreact

--- init.lua
+++ init.lua
@@ -109,11 +109,14 @@ require("lazy").setup({
 		event = "InsertEnter",
 		dependencies = { "rafamadriz/friendly-snippets" },
 		config = function()
-			-- local luasnip = require("luasnip")
+			local luasnip = require("luasnip")
 			-- luasnip.filetype_extend("htmldjango", { "html" })
-			-- luasnip.filetype_extend("typescript", { "javascript" })
-			require("luasnip/loaders/from_vscode").lazy_load({ paths = "~/.config/nvim/snippets/" })
-			require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets/" })
+			luasnip.filetype_extend("typescriptreact", { "html" })
+			luasnip.filetype_extend("javascriptreact", { "html" })
+			-- require("luasnip/loaders/from_vscode").lazy_load({ paths = "~/.config/nvim/snippets/" })
+			require("luasnip/loaders/from_vscode").lazy_load()
+			-- enable it if you will define lua snippets
+			-- require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets/" })
 		end,
 		build = "make install_jsregexp",
 	},
