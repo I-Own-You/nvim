f42b194f2d77ef98bce95f9d4320de1c0ea49b29
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2025-08-15 04:12:51 +0300

    added treeewalker plugin

---
+++ lua/plugins/treewalker.lua
@@ -0,0 +1,26 @@
+return {
+	"aaronik/treewalker.nvim",
+	opts = {
+		-- How long should above highlight last (in ms)
+		highlight_duration = 250,
+
+		-- Whether to briefly highlight the node after jumping to it
+		highlight = true,
+
+		-- The color of the above highlight. Must be a valid vim highlight group.
+		-- (see :h highlight-group for options)
+		highlight_group = "CursorLine",
+	},
+	config = function()
+		-- movement
+		vim.keymap.set({ "n", "v" }, "<C-k>", "<cmd>Treewalker Up<cr>", { silent = true })
+		vim.keymap.set({ "n", "v" }, "<C-j>", "<cmd>Treewalker Down<cr>", { silent = true })
+		vim.keymap.set({ "n", "v" }, "<C-h>", "<cmd>Treewalker Left<cr>", { silent = true })
+		vim.keymap.set({ "n", "v" }, "<C-l>", "<cmd>Treewalker Right<cr>", { silent = true })
+		-- swapping
+		vim.keymap.set("n", "<C-S-k>", "<cmd>Treewalker SwapUp<cr>", { silent = true })
+		vim.keymap.set("n", "<C-S-j>", "<cmd>Treewalker SwapDown<cr>", { silent = true })
+		vim.keymap.set("n", "<C-S-h>", "<cmd>Treewalker SwapLeft<cr>", { silent = true })
+		vim.keymap.set("n", "<C-S-l>", "<cmd>Treewalker SwapRight<cr>", { silent = true })
+	end,
+}
