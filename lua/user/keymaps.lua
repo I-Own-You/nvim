7fe983469d591dcbeab51c7ba6463d954058c72e
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2025-09-23 06:31:49 +0300

    fixed path for keymap after moved file

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -85,7 +85,7 @@ keymap("n", "<leader>nt", ":tabnew<CR>", { silent = true, desc = "open new tab" 
 -- "%:p:h" without filename
 keymap("n", "<Leader>xt", ':let @+=expand("%:p")<CR>', { silent = true, desc = "copy current buffers location" })
 
-keymap("n", "q", ":lua require('utility_functions').close_file_type_buffers()<CR>", { silent = true })
+keymap("n", "q", ":lua require('user.utility_functions').close_file_type_buffers()<CR>", { silent = true })
 
 -- this is for LSP builtin keymaps to remove them
 vim.keymap.del("n", "grn")
