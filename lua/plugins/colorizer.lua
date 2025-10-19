008833b657cbf850275ed79677ff338ecb8676eb
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2025-10-19 19:55:48 +0300

    enabled all colors to show

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,6 +1,6 @@
 local opts = {
-	filetypes = {
-		"lua",
+	-- filetypes = {
+	-- 	"lua",
 		-- "typescript",
 		-- "javascript",
 		-- "typescriptreact",
@@ -9,17 +9,17 @@ local opts = {
 		-- "scss",
 		-- "sass",
 		-- "text",
-	},
+	-- },
 	user_default_options = {
 		RGB = true, -- #RGB hex codes
 		RRGGBB = true, -- #RRGGBB hex codes
 		names = true, -- "Name" codes like Blue or blue
-		RRGGBBAA = false, -- #RRGGBBAA hex codes
-		AARRGGBB = false, -- 0xAARRGGBB hex codes
-		rgb_fn = false, -- CSS rgb() and rgba() functions
-		hsl_fn = false, -- CSS hsl() and hsla() functions
-		css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-		css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+		RRGGBBAA = true, -- #RRGGBBAA hex codes
+		AARRGGBB = true, -- 0xAARRGGBB hex codes
+		rgb_fn = true, -- CSS rgb() and rgba() functions
+		hsl_fn = true, -- CSS hsl() and hsla() functions
+		css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+		css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
 		-- Available modes for `mode`: foreground, background,  virtualtext
 		mode = "background", -- Set the display mode.
 		-- Available methods are false / true / "normal" / "lsp" / "both"
