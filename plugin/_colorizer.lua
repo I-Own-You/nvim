254e2f072db00fe829e3469f26a9692b8ec8b4ed
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2026-05-21 23:43:55 +0300

    disabled colirzed plugin

--- plugin/_colorizer.lua
+++ plugin/_colorizer.lua
@@ -1,5 +1,5 @@
-vim.pack.add({ "https://github.com/NvChad/nvim-colorizer.lua" })
-require("colorizer").setup({
+-- vim.pack.add({ "https://github.com/NvChad/nvim-colorizer.lua" })
+return {} or require("colorizer").setup({
 	-- filetypes = {
 	-- "lua",
 	-- "typescript",
@@ -14,11 +14,26 @@ require("colorizer").setup({
 	options = {
 		parsers = {
 			rgb = true,
+			hsl = true,
+			oklch = true,
+			hwb = true,
+			lab = true,
+			lch = true,
+			css_color = true,
 			names = true,
 			css = true,
 			css_fn = true,
+			hex = true,
 			tailwind = { enable = false },
-			sass = { enable = true, parsers = { "css" } },
+			sass = { enable = false, parsers = { "css" } },
+			xterm = { enable = false }, -- xterm 256-color codes (#xNN, \e[38;5;NNNm)
+			xcolor = true,
+			hsluv = true,
+			css_var_rgb = true,
+			css_var = {
+				enable = true, -- resolve var(--name) references to their defined color
+				parsers = { css = true }, -- parsers for resolving variable values
+			},
 		},
 	},
 	display = {
