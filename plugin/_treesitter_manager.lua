fc0abfc20eccbab4421ea5ac4b038ecefa799e17
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2026-06-19 21:56:43 +0300

    added new ts lang

--- plugin/_treesitter_manager.lua
+++ plugin/_treesitter_manager.lua
@@ -1,7 +1,7 @@
 vim.pack.add({ "https://github.com/romus204/tree-sitter-manager.nvim" })
 
 require("tree-sitter-manager").setup({
-	ensure_installed = { "lua", "c", "cpp", "toml", "kdl", "python", "html", "css", "javascript", "typescript"  }, -- list of parsers to install at the start of a neovim session
+	ensure_installed = { "lua", "c", "cpp", "toml", "kdl", "python", "html", "css", "javascript", "typescript", "commonlisp"  }, -- list of parsers to install at the start of a neovim session
 	-- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
 	auto_install = false, -- if enabled, install missing parsers when editing a new file
 	highlight = true,
