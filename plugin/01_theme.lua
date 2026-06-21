ce472f53e1464f7cfcfddf6d88f6b108143bd9d0
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2026-06-21 19:43:49 +0300

    changed normalfloat highlight for theme

--- plugin/01_theme.lua
+++ plugin/01_theme.lua
@@ -44,3 +44,4 @@ vim.cmd.colorscheme("srcery")
 
 -- vim.api.nvim_set_hl(0, 'Visual', { bg = '#282828', force = true })
 vim.api.nvim_set_hl(0, 'Visual', { bg = '#2e251e', force = true })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#111111", force = true })
