f369b41dc6a8e2502e6b71e2702bbff5f30ca5f2
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2026-06-25 03:07:11 +0300

    fixed ctrl-b issue for blink cmp

--- plugin/LSP_01_blink_cmp.lua
+++ plugin/LSP_01_blink_cmp.lua
@@ -55,15 +55,24 @@ require("blink-cmp").setup({
 		["<C-n>"] = { "select_next" },
 		["<C-k>"] = { "select_prev" },
 		["<C-j>"] = { "select_next" },
-		["<C-b>"] = { "scroll_documentation_up", "fallback" },
+		-- ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+		["<C-b>"] = {
+			function(cmp)
+				if cmp.is_documentation_visible() then
+					cmp.scroll_documentation_up(4) -- scroll by 4 up
+					return true -- dont give ^B into buffer
+				end
+			end,
+			"fallback",
+		},
 		["<C-f>"] = { "scroll_documentation_down", "fallback" },
 		["<TAB>"] = { "select_next", "snippet_forward", "fallback" },
 		["<S-TAB>"] = { "select_prev", "snippet_backward", "fallback" },
 	},
 	completion = {
 		menu = {
-            -- min_width = 100,
-            max_height = 9999,
+			-- min_width = 100,
+			max_height = 9999,
 			draw = {
 				columns = {
 					{ "label", "label_description", gap = 1 },
