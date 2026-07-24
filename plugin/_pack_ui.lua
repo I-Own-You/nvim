c54664f18eaf101ff3fc812f8bc7bb7101681f5b
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2026-07-24 03:36:26 +0300

    removed a plugin and added a nnew one

---
+++ plugin/_pack_ui.lua
@@ -0,0 +1,11 @@
+vim.pack.add({ "https://github.com/jtprogru/pack-ui.nvim" })
+
+require("pack_ui").setup({
+	-- border = "rounded", -- any nvim_open_win border
+	-- title = " vim.pack ",
+	-- max_width = 100,
+	-- width_ratio = 0.9,
+	-- height_ratio = 0.85,
+	-- auto_check = false, -- on setup, check remotes and notify if updates exist
+	-- auto_update = false, -- on setup, apply every available update automatically
+})
