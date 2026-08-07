4aa040afbf41b5596e982d5f6e866e02eb6f2853
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2026-08-07 23:25:15 +0300

    removed a highlight i dont need

--- plugin/01_theme.lua
+++ plugin/01_theme.lua
@@ -1,7 +1,6 @@
 vim.pack.add({ "https://github.com/srcery-colors/srcery-vim" })
 vim.cmd.colorscheme("srcery")
 
-vim.api.nvim_set_hl(0, "Visual", { bg = "#282828", force = true })
 vim.api.nvim_set_hl(0, "Visual", { bg = "#2e251e", force = true })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#111111", force = true })
 vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#2e251e", force = true })
