a2fe903f30455bb13045623d8c37a11ace78b430
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2026-08-08 16:09:51 +0300

    added new autocmd to not create comment on new line after "enter"

--- lua/user/auto_commands.lua
+++ lua/user/auto_commands.lua
@@ -81,7 +81,12 @@ vim.api.nvim_create_user_command("LspInfo", function()
 	end
 end, {})
 
-vim.cmd("cabbrev tabrename TabRename")
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "r", "o" })
+  end,
+})
 
 vim.cmd("cabbrev lspstart LspStart")
 vim.cmd("cabbrev lsprestart LspRestart")
