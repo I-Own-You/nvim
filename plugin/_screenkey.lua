8f1bbdf289460e54c85410bb07f19194dc80c116
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2026-08-19 15:45:49 +0300

    formatted file

--- plugin/_screenkey.lua
+++ plugin/_screenkey.lua
@@ -1,7 +1,7 @@
 vim.pack.add({
-    {
-        src = "https://github.com/nstefan002/screenkey.nvim",
-        version = vim.version.range("*"),
-    }
+	{
+		src = "https://github.com/nstefan002/screenkey.nvim",
+		version = vim.version.range("*"),
+	},
 })
 require("screenkey").setup({})
